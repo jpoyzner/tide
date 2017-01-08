751413e73c7e00d191d5372b50e1b22fd0e4b0ad
Jeff Poyzner <holidaydoctor@hotmail.com>
2017-01-08 11:38:49 -0800

    better than jaws

--- best.js
+++ best.js
@@ -1,6 +1,10 @@
 var http = require('http');
+var fs = require('fs');
 var tickers = require('./tickers');
-var movements = {};
+var newPrices = {};
+var oldPrices = {};
+
+const MAX_MOVE = 0.016;
 
 var getStock = function(tickerIndex, callback) {
 	if (tickerIndex >= tickers.length) {
@@ -28,50 +32,99 @@ var getStock = function(tickerIndex, callback) {
 				
 				var newPrice = Number(data_object[0].l_fix);
 				console.log(ticker + " NEW PRICE: " + newPrice);
-				var twentyAgo = new Date();
-				twentyAgo -= 20 * 60000;
-				http.get({host: 'www.google.com', port: 80, path: '/finance/getprices?x=NASDAQ&p=1d&i=60&ts=' + +twentyAgo + '&f=h&q=' + ticker}, function(response) {
-						response.setEncoding('utf8');
-						var data = "";
-									
-						response.on('data', function(chunk) {
-							data += chunk;
-						});
-						
-						response.on('end', function() {
-							if (data.length > 0) {
-								var lines = data.split('\n');
-								var oldPrice = parseFloat(lines[lines.length - 2]);
-							    console.log(ticker + " OLD PRICE: " + oldPrice);
-							    movements[ticker] = (newPrice - oldPrice) / oldPrice;
-							    console.log(ticker + " MOVEMENT: " + movements[ticker]);
-								
-								getStock(++tickerIndex, callback);
-							} else {
-								callback();
-							}
-						});
-					});
+				newPrices[ticker] = newPrice;
+				getStock(++tickerIndex, callback);
+				
+//				var twentyAgo = new Date();
+//				twentyAgo -= 20 * 60000;
+//				http.get({host: 'www.google.com', port: 80, path: '/finance/getprices?x=NASDAQ&p=1d&i=60&ts=' + +twentyAgo + '&f=h&q=' + ticker}, function(response) {
+//						response.setEncoding('utf8');
+//						var data = "";
+//									
+//						response.on('data', function(chunk) {
+//							data += chunk;
+//						});
+//						
+//						response.on('end', function() {
+//							if (data.length > 0) {
+//								var lines = data.split('\n');
+//								var oldPrice = parseFloat(lines[lines.length - 2]);
+//							    console.log(ticker + " OLD PRICE: " + oldPrice);
+//							    movements[ticker] = (newPrice - oldPrice) / oldPrice;
+//							    console.log(ticker + " MOVEMENT: " + movements[ticker]);
+//								
+//								getStock(++tickerIndex, callback);
+//							} else {
+//								callback();
+//							}
+//						});
+//					});
 			} else {
 				callback();
 			}
 		});
 	});
-}	
+}
 
-getStock(0, function() {
-	var gain = 0;
-	var bestMover;
-	var bestMovement = 0; //in percent
-	
-	tickers.forEach(function(ticker) {
-		var movement = movements[ticker.ticker];
-		//console.log(ticker.ticker + " MOVEMENT: " + movement + ", SOFAR: " + bestMovement);
-		if (movement >= bestMovement) {
-			bestMover = ticker.ticker;
-			console.log("NEW BEST MOVER: " + ticker.ticker + ", MOVEMENT: " + movement);
+function formatPrices() {
+	var output = "";
+	Object.keys(newPrices).forEach(function(ticker) {
+		output += ticker + " " + newPrices[ticker] + "|";
+	});	
+	return output;
+}
+
+function readPrices(callback) {
+	fs = require('fs')
+	fs.readFile('prices.txt', 'utf8', function (err, data) {
+		if (err) {
+			console.log(err)
+			callback();
+			return;
 		}
+	  
+	  	oldPrices = {};
+		data.split('|').forEach(function(price) {
+			var priceParts = price.split(' ');
+			oldPrices[priceParts[0]] = priceParts[1];
+		});
+		
+		callback();
 	});
-	
-	console.log("BEST MOVER: " + bestMover + ", MOVEMENT: " + bestMovement + "%");
+}
+
+getStock(0, function() {  
+    readPrices(function() {
+    	var gain = 0;
+    	var bestMover;
+    	var bestMovement = 0; //in percent
+    	
+    	Object.keys(newPrices).forEach(function(ticker) {
+    		var newPrice = newPrices[ticker] || 0;
+    		console.log(ticker + " NEW PRICE: " + newPrice);
+    		var oldPrice = oldPrices[ticker] || 0
+    		console.log(ticker + " OLD PRICE: " + oldPrice);
+    		var movement = (newPrice - oldPrice) / oldPrice;
+    		console.log(ticker + " MOVEMENT: " + movement + ", BEST SO FAR: " + bestMovement * 100 + "%");
+    		if (movement > bestMovement && movement < MAX_MOVE) {
+    			bestMover = ticker;
+    			bestMovement = movement;
+    			console.log("NEW BEST MOVER: " + ticker + ", MOVEMENT: " + movement);
+    		}
+    	});
+    	
+    	if (bestMover) {
+    		console.log("\nBUY: " + bestMover + " ON MOVEMENT: " + bestMovement * 100 + "%");
+    	} else {
+    		console.log("\nSELL EVERYTHING!!!")
+    	}
+    	
+    	fs.writeFile("prices.txt", formatPrices(), function(err) {
+    	    if (err) {
+    	        return console.log(err);
+    	    }
+
+    	    //console.log("The file was saved!");
+    	});
+    });
 });
--- history.js
+++ history.js
@@ -1,102 +1,14 @@
 //http://www.networkerror.org/component/content/article/1-technical-wootness/44-googles-undocumented-finance-api.html
 
 var http = require('http');
-var tickers = require('./tickers');
+var tickers = require('./tickers'); //USING NASDAQ 100
 
-//var googleFinance = require('google-finance');
-//
-//var date = new Date();
-//date.setHours(6);
-//date.setMinutes(30);
-//date.setDate(6);
-//
-//var endDate = new Date();
-//endDate.setHours(13);
-//endDate.setMinutes(30);
-//endDate.setDate(6);
+const MINUTE_INTERVAL = 20;
+const MAX_MOVE = 0.016;
+const NUM_STOCKS = 1;
 
-
-//USING NASDAQ 100:
-
-/*
- * FRIDAY
- * 1min: 1.2%
- * 2min: 3.1%
- * 5min: 1.18%
- * 10min: 0.7%
- * 15min: -0.3%
- * 20min: 2.2%
- * 25min: 1.9%
- * 30min: -.9%
- * 60min: 0.9%
- */
-
-/*
- * 3 weeks, 1 stock
- * 1min: 12.2%
- * 2min: 29.1%
- * 5min: 38.6%
- * 10min: 30.4%
- * 15min: 29.5%
- * 20min: 32.6%
- * 25min: 25.4%
- * 30min: 7.3%
- * 60min: -4%
- * 2hours: -6.7%
- */
-
-/*
- * 3 weeks, 2 stocks
- * 1min: 20.3%
- * 2min: 41.3%
- * 5min: 33.1%
- * 10min: 17%
- * 15min: 22%
- * 20min: 25.5%
- * 25min: 18.1%
- * 30min: 7.3%
- * 60min: 2.6%
- * 2hours: 0.04%
- */
-
-/*
- * 3 weeks, 3 stocks
- * 1min: 27.8%?
- * 2min: 43.9%
- * 5min: 29.5% 
- * 10min: 18.3%
- * 15min: 18.3%
- * 20min: 19.1%
- * 25min: 15%
- * 30min: 5.6%
- */
-
-/*
- * 3 weeks, 4 stocks
- * 1min: 32.4%
- * 2min: 43.5%
- * 5min: 28.4%
- * 10min: 15.9%
- * 15min: 14.4%
- * 20min: 
- * 25min: 
- * 30min: 
- */
-
-/*
- * 3 weeks, 5 stocks
- * 1min: 35.2%
- * 2min: 43.2%
- * 5min: 26.3%
- * 10min: 18.6%
- * 15min: 13.7%
- * 20min: 
- * 25min: 
- * 30min: 
- */
-
-var totalMins = 4922 * 60;
-var timeFrame = 20 * 60;
+var totalMins = 4922 * 60; //TODO: can this change?
+var timeFrame = MINUTE_INTERVAL * 60;
 
 var history = {};
 
@@ -110,7 +22,6 @@ var getStock = function(tickerIndex, callback) {
 	history[ticker] = [];
 	
 	http.get({host: 'www.google.com', port: 80, path: '/finance/getprices?x=NASDAQ&i=' + timeFrame + '&p=15d&f=h&q=' + ticker}, function(response) {
-	//googleFinance.historical({symbol: ticker, from: date}, function(err, quotes) {
 		response.setEncoding('utf8');
 		var data = "";
 					
@@ -127,7 +38,6 @@ var getStock = function(tickerIndex, callback) {
 			    	history[ticker].push(parseFloat(lines[line]));
 			    }
 				
-				//date.setMinutes(date.getMinutes() + 1);
 				getStock(++tickerIndex, callback);
 			} else {
 				callback();
@@ -156,20 +66,26 @@ getStock(0, function() {
 		
 		console.log("OLD BEST MOVERS: " + bestMovers.join(',') + ", GAINED: " + (gainTotal / bestMovers.length * 100) + "%");
 		
-
+		bestMovers.forEach(function(bestMover) {
+			console.log(bestMover + " PRICE: " + history[bestMover][i]);
+		});
+		
 		console.log("SLICE " + i + " ---------------------------------------------------------")
 		tickers.forEach(function(ticker) {
 			var price = history[ticker.ticker][i];
 			if (i > 0) {
-				bestMovers.push(ticker.ticker);
-				
-				bestMovers.sort(function(a, b) {
-					var lastPriceA = history[a][i - 1];
-					var lastPriceB = history[b][i - 1];
-					return (history[b][i] - lastPriceB) / lastPriceB - (history[a][i] - lastPriceA) / lastPriceA;
-				});
-				
-				bestMovers = bestMovers.slice(0, 1);
+				var oldPrice = history[ticker.ticker][i - 1]
+				if ((price - oldPrice) / oldPrice < MAX_MOVE) {
+					bestMovers.push(ticker.ticker);
+					
+					bestMovers.sort(function(a, b) {
+						var lastPriceA = history[a][i - 1];
+						var lastPriceB = history[b][i - 1];
+						return (history[b][i] - lastPriceB) / lastPriceB - (history[a][i] - lastPriceA) / lastPriceA;
+					});
+					
+					bestMovers = bestMovers.slice(0, NUM_STOCKS);
+				}
 			}
 		});
 		
@@ -180,6 +96,11 @@ getStock(0, function() {
 		});
 		
 		console.log("NEW BEST MOVERS: " + bestMovers.join(',') + ", MOVEMENT: " + (avgMovement / bestMovers.length * 100) + "%");
-		console.log("---->RUNNING GAIN: " + (gain * 100) + "%");
+		
+		bestMovers.forEach(function(bestMover) {
+			console.log(bestMover + " PRICE: " + history[bestMover][i]);
+		});
+		
+		console.log("--------------------------------------------------------------------->RUNNING GAIN: " + (gain * 100) + "%");
 	}
 });
